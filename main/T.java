8f6b28c9157f850b8d2ba55f35b2130f400456d2
gibsonw <gibsonw@gmail.com>
2020-05-07 22:16:36 -0300

    add T

---
+++ main/T.java
@@ -0,0 +1,22 @@
+package main;
+
+import java.util.logging.Logger;
+import java.util.ArrayList;
+/*
+import lists.OptimisticList;
+import lists.FineList;
+import lists.CoarseList3;
+*/
+import java.util.Random;
+import java.time.Duration;
+import java.time.Instant;
+import java.io.*;
+
+public class T{
+
+    public static void main(String[] args) {
+        System.out.println("x");
+
+
+    }
+}
